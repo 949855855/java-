e0394aebf9aa8168528fe3e5af45151376e517d2
94985585 <ibratqodirov001@gmail.com>
2023-01-26 17:47:52 +0500

    jhsakjs

---
+++ main.js
@@ -0,0 +1,132 @@
+    //  function checkincr (num) {
+    //     if (num > 0){
+    //         num += 5
+    //     } else {
+    //         num -=2
+    //     }
+    //     return num
+    //  }
+
+
+   //   function ibratkadirov(x,y){
+   //      return x + y
+   //   }
+
+
+   
+   //   ibratkadirov(4,6);
+
+   // function CheckPositive(num) {
+   //    if (num > 0){
+   //       console.log(true);
+   //    } else if(num === 0) {
+   //       console.log(nolga teng);
+   //    } else {
+   //       console.log(folse);
+   //    }
+   // }
+
+   // CheckPositive(-0)
+
+
+
+// function checkodd(num){
+//    if (num % 2 !== 0 ){
+//       console.log(odd number);
+//    } else {
+//       console.log(evon number);
+//    }
+// }
+
+// checkodd(0)
+
+
+// function checkab(a,b) {
+//    if (a > 0 && b > 0) {
+//       console.log(true);
+//    } else {
+//       console.log(false);
+//    }
+// }
+
+
+// var a = 10;
+
+// var b = 6;
+
+// if (a % !=== 0 && b % 2 !== 0){
+//    console.log(true);
+// }
+
+
+// function ckeck (a , b, c){
+//    if (a > 0 && b > 0 || c > 0) {
+
+//    } else if (a > 0 || b < 0 || c > 0 ){
+
+//    } else if (a > 0 || b < 0 || c > 0 ) {
+       
+//    }
+// }
+
+
+// function ckeck incrment(num){
+//    if (num > 0) {
+//       num ++;
+//    } else {
+//       num--
+//    }
+//    return num
+// }
+
+
+// function checkincr(num){
+//    if (num > 0 ){
+//       num += 3
+//    } else {
+//       num -=2
+//    } 
+//    return num
+// }
+
+
+//    var person = {
+//       name:'Ibrat',
+//       age : null,
+//       contat: '94 985 58 55',
+//       adress: "Tashkent",
+
+//    }
+
+//    var student = ['Ibrat',"shamsiddin","saloxiddin","Mavlonbek","Sardorbek","Asrorbek","Jamshidbek","Xurshidbek","Ilhombek","Odiljon", "Uligbek","Rozali",]
+
+//   function logstudent(arr){
+//    for (var i = 0; i < 16; i++ ){
+//       console.log(arr[i]);
+//    }
+//   }
+
+
+//   logstudent(student)
+
+
+
+// var emptyArr = [];
+
+// emptyArrmp.push ("ibrat")
+// function pushArr(qiymat){
+//    emptyArr.pushArr(qiymat){
+//       emptyArr.push(qiymat)
+//    }
+// }
+
+// pushArr(3)
+
+
+// var javon = [];
+
+// function kitobQosh(kitobnomi) {
+//    javon.push(kitobnomi)
+// }
+
+// kitobQosh("alvido bolalik")
